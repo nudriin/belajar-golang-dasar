806599956487fb054c5d015857c08f02fdddd80b
Nurdin <mrsunnysummer@gmail.com>
2024-08-07 02:04:07 +0700

    Update chapter 6 type declaration

--- ch_6_type_declaration.go
+++ ch_6_type_declaration.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	//! membuat tipe data NoKTP yang bertipe string
 	type NoHP string
 
-	var name NoHP = "081549193834"
+	var phone NoHP = "081549193834"
+
+	var phoneString string = "08213322"
+	var phoneCast = NoHP(phoneString) //! casting strinng ke NoHp
+	fmt.Println(phone)
+	fmt.Println(phoneCast)
 }
